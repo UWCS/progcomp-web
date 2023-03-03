2abf2b30af1e40e4d91e45e9a55455142bf9a993
ericthelemur <owenconnors1@gmail.com>
2023-03-03 15:51:39 +0000

    Re select last selected test from the last selected problem

---
+++ progcomp/static/keep_selected.js
@@ -0,0 +1,26 @@
+var test_select_elem;
+
+const selected = function () {
+    const [last_prob, last_test] = [Cookies.get("last_prob"), Cookies.get("last_test")];
+    console.log(last_prob, last_test);
+    if (window.location.pathname == "/problems/" + last_prob) {
+        test_select_elem.selectedIndex = last_test;
+    }
+}
+
+dark();
+
+// Set toggle button on load
+window.addEventListener('load', function () {
+    test_select_elem = document.getElementById("test_select");
+    selected();
+
+    test_select_elem.addEventListener('change', () => {
+        console.log("Loeaded")
+        const prob = window.location.pathname.split("/")[2];
+        Cookies.set("last_prob", prob, { SameSite: "Strict" });
+        const test = test_select_elem.selectedIndex;
+        Cookies.set("last_test", test, { SameSite: "Strict" });
+        console.log(prob, test);
+    });
+});
