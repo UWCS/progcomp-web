1d51f4fc04d162fb58511b48cbdea0e5bcec39bc
ericthelemur <owenconnors1@gmail.com>
2023-03-03 12:34:33 +0000

    Darkmode set is no longer deferred

--- progcomp/static/darkmode.js
+++ progcomp/static/darkmode.js
@@ -3,10 +3,13 @@ const dark = function () {
     document.documentElement.setAttribute('data-bs-theme', curr_dark ? "dark" : 'light');
 }
 
-document.getElementById('dark-mode').addEventListener('click', () => {
-    const curr_dark = Cookies.get("dark_mode") == "dark";
-    Cookies.set("dark_mode", curr_dark ? "light" : "dark", { SameSite: "None" });
-    dark();
-});
-
 dark();
+
+// Set toggle button on load
+window.addEventListener('load', function () {
+    document.getElementById('dark-mode').addEventListener('click', () => {
+        const curr_dark = Cookies.get("dark_mode") == "dark";
+        Cookies.set("dark_mode", curr_dark ? "light" : "dark", { SameSite: "Strict" });
+        dark();
+    });
+});
