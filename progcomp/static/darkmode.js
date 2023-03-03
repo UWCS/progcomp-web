5c02eeb3e45603d4fbb930272b69dc075bdcd558
ericthelemur <owenconnors1@gmail.com>
2023-03-03 02:40:59 +0000

    Dark mode toggle

---
+++ progcomp/static/darkmode.js
@@ -0,0 +1,8 @@
+document.getElementById('dark-mode').addEventListener('click', () => {
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    }
+})
