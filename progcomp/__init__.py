062daa2026b8bf6fcdc18e3ba972161713ec1ec4
Thomas Facos <tjfacos@gmail.com>
2025-01-01 22:55:39 +0000

    Updates secret key to a random string, and made darkmode icon change.

--- progcomp/__init__.py
+++ progcomp/__init__.py
@@ -3,6 +3,8 @@ import os
 
 from flask import Flask
 
+from uuid import uuid4 as uuid
+
 from .database import db, migrate
 
 logging.basicConfig(
@@ -18,7 +20,7 @@ logging.basicConfig(
 def create_app() -> Flask:
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
+        SECRET_KEY=uuid().hex,
         MAX_CONTENT_LENGTH=1000 * 1000 * 1000,  # 1gb,
         SQLALCHEMY_DATABASE_URI=os.environ["DATABASE_URL"],
     )
--- progcomp/static/darkmode.js
+++ progcomp/static/darkmode.js
@@ -1,12 +1,18 @@
-const dark = function () {
-    const curr_dark = Cookies.get("dark_mode") == "dark";
-    document.documentElement.setAttribute('data-bs-theme', curr_dark ? "dark" : 'light');
+const dark = function (not_loaded = false) {
+    const curr_light = Cookies.get("dark_mode") == "light";
+    document.documentElement.setAttribute('data-bs-theme', curr_light ? "light" : 'dark');
+    
+    if (not_loaded) return
+
+    document.getElementById("dark-mode-icon").classList.add(curr_light ? "bi-moon-fill" : "bi-sun-fill")
+    document.getElementById("dark-mode-icon").classList.remove(curr_light ? "bi-sun-fill" : "bi-moon-fill")
 }
 
-dark();
+dark(true);
 
 // Set toggle button on load
 window.addEventListener('load', function () {
+    dark()
     document.getElementById('dark-mode').addEventListener('click', () => {
         const curr_dark = Cookies.get("dark_mode") == "dark";
         Cookies.set("dark_mode", curr_dark ? "light" : "dark", { SameSite: "Strict" });
